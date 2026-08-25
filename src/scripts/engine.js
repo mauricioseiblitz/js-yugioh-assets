150cec4db7eb94123894c203397fa59c771c35b1
mauricioseiblitz <mauricio-monteiro@hotmail.com.br>
2026-08-25 20:29:20 -0300

    Implementando o setCardFields

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -66,18 +66,36 @@ async function createCardImage(IdCard, fieldSide) {
 
     if (fieldSide === playerSides.player1) {
         cardImage.addEventListener("mouseover", () => {
-            drawSelectedCard(IdCard);
+            drawSelectCard(IdCard);
         });
 
         cardImage.addEventListener("click", () => {
-            setCardsField(cardImage.getAtrribute("data-id"));
+            setCardsField(cardImage.getAtribute("data-id"));
         });
     }
 
     return cardImage;
 }
 
-async function drawSelectedCard(index) {
+async function setCardsField(cardId) {
+    await removeAllCardsImages();
+
+    let computerCardId = await getRandomCardId();
+
+    state.fieldCards.player.style.display = "block";
+    state.fieldCards.computer.style.display = "block";
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResults = await checkDuelResults(cardId, computerCardId);
+
+    await updateScore();
+    await drawButton(duelResults);
+
+}
+
+async function drawSelectCard(index) {
     state.cardSprites.avatar.src = cardData[index].img;
     state.cardSprites.name.innerText = cardData[index].name;
     state.cardSprites.type.innerText = "Attribute: " + cardData[index].type;
